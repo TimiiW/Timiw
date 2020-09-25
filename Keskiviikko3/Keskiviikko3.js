42bc918c7a274f49de628b0060b31ebfc3858538
unknown <timi.wahlman@edu.keuda.fi>
2020-09-25 11:14:51 +0300

    ke3

---
+++ Keskiviikko3/Keskiviikko3.js
@@ -0,0 +1,8 @@
+function Nappi() {
+
+var parillinen = document.getElementById('parillinen').value;
+if (parillinen % 2 == 0)
+{document.write('Numero on parillinen');}
+else
+{document.write('Numero on parition');}
+}
---
+++ TestiFolder/Testi1.js
@@ -0,0 +1,7 @@
+let number = prompt("Numero");
+if(number % 2 == 0) {
+    console.log("Numero on parillinen");
+}
+else {
+    console.log("Numero on pariton");
+}
