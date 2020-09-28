65e5e28a0dde1456c34b1bd55793bc8ce6fad24e
unknown <timi.wahlman@edu.keuda.fi>
2020-09-28 10:20:10 +0300

    Pe1

---
+++ Perjantain Tehtävät/Perjantai1/Perjantai1.js
@@ -0,0 +1,9 @@
+function nappi() {
+var numero = parseInt(document.getElementById("numero").value);
+if (numero > 0) {
+  document.write('Numero on posiivinen')
+}
+else {
+  document.write ('numero on negatiivinen')
+}
+}
